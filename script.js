34c27a6f6ae5efe4607c2b2483047af17fcf7875
Anya McMahon <anyamariemusic@gmail.com>
2022-11-11 19:35:54 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Global variables
+var crust 
+
+// toppings array
+
+// event listener
+
+
+function calculateTotal(toppingArray) {  
+    let total = 0; 
+    let toppingCost = 2.50; 
+    let baseCost = 5.50; // cost of crust and sauce
+
+    // order string concatenation
+    
+
+    let toppingString = "Toppings: ";
+
+    // For Loop
+
+    // total = baseCost + cost of all toppings
+
+    // set DOM total += total   
+    // use DOM: = orderString   
+    // use DOM: = toppingString 
+}
